6d9a5b92268d76830c7f12b29a4f96b135139d13
Mohammad Ali Khan <maktweet@gmail.com>
2021-09-20 23:49:44 +0600

    Update Employee.java

--- JavaTest/src/main/java/Employee.java
+++ JavaTest/src/main/java/Employee.java
@@ -1,6 +1,6 @@
 public class Employee {
 
     public static void main(String[] args){
-        System.out.println("Java Project");
+        System.out.println("Java Project with poll scm");
     }
 }
